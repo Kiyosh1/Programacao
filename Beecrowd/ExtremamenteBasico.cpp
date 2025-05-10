5b9c78d3b6e1811195382045a3670b8a181027ad
Felipe Kiyoshi <94464410+Kiyosh1@users.noreply.github.com>
2025-05-10 19:00:47 -0300

    Create ExtremamenteBasico.cpp

---
+++ Beecrowd/ExtremamenteBasico.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  int a, b, soma;
+
+  std::cin >> a;
+  std::cin >> b;
+
+  soma = a + b;
+
+  std::cout << "X = " << soma << '\n';
+
+  return 0;
+}
