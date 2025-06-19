c5e07633d7d5f2f184a9f5caf3dcf0eb9f16ad10
Felipe Kiyoshi <94464410+Kiyosh1@users.noreply.github.com>
2025-06-19 20:56:16 -0300

    Add files via upload

---
+++ Beecrowd/hohoho.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class hohoho {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i=0; i<N-1; i++){
+            System.out.print("Ho ");
+        }
+            System.out.printf("Ho!\n");
+
+        sc.close();
+    }
+}
