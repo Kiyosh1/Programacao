6288b925d18edcc6b06d4791d02dc12a92e7f55a
kiyoshi <kiyoshii@protonmail.com>
2025-07-24 23:54:55 -0300

    Saida 1

---
+++ Beecrowd/Saida1.java
@@ -0,0 +1,13 @@
+
+public class Saida1 {
+    public static void main(String[] args) {
+        
+        String a = "---------------------------------------";
+        String b = "|                                     |";
+
+        System.out.println(a);
+        for(int i=0; i<5; i++)
+            System.out.println(b);
+        System.out.println(a);
+    }
+}
