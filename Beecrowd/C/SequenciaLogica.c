9f2beb63678fa62a5e57e753ed7b378492f4f255
kiyoshi <kiyoshii@protonmail.com>
2025-09-17 22:12:32 -0300

    1145

---
+++ Beecrowd/C/SequenciaLogica.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int x;
+  int y;
+
+  scanf("%d", &x);
+  scanf("%d", &y);
+
+  for (int i = 1; i <= y; i++) {
+
+    printf("%d", i);
+    if (i % x == 0) {
+      printf("\n");
+    } else {
+      printf(" ");
+    }
+  }
+}
