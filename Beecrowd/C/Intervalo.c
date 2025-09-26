2deb36c232c59a264d5596541b2ae1d99eeed139
kiyoshi <kiyoshii@protonmail.com>
2025-09-25 22:48:06 -0300

    1037

---
+++ Beecrowd/C/Intervalo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+  double valor;
+  scanf("%lf", &valor);
+
+  if (valor > 100 || valor < 0) {
+    printf("Fora de intervalo\n");
+  } else if (valor <= 25) {
+    printf("Intervalo [0,25]\n");
+  } else if (valor <= 50) {
+    printf("Intervalo (25,50]\n");
+  } else if (valor <= 75) {
+    printf("Intervalo (50,75]\n");
+  } else {
+    printf("Intervalo (75,100]\n");
+  }
+}
