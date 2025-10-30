d4a6336b9e4dbb21b7203fb8bb443e38896c176b
kiyoshi <kiyoshii@protonmail.com>
2025-10-29 23:44:41 -0300

    1182

---
+++ Beecrowd/ColunaNaMatriz.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class ColunaNaMatriz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j;
+        double S = 0; 
+        double M = 0;
+
+        double[][] mat = new double[12][12];
+
+        int C = sc.nextInt(); 
+
+        char T = sc.next().charAt(0);
+
+        for(i = 0; i < 12; i++){
+            for(j = 0; j < 12; j++){
+                mat[i][j] = sc.nextDouble(); 
+            }
+        }
+
+        for(i = 0; i < 12; i++){
+            S += mat[i][C];
+            M += mat[i][C];
+        }
+
+        if(T == 'S'){
+            System.out.printf("%.1f\n", S);
+        }
+        else if (T == 'M'){ 
+            M = M / 12.0;
+            System.out.printf("%.1f\n", M);
+        }
+
+        sc.close();
+    }
+}
+
